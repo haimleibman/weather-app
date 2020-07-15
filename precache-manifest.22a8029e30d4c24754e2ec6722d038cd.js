ea3df8a369d563e3d60b6a1813880da2745648c6
חיים לייבמן <hnl770@gmail.com>
2020-07-15 16:40:26 +0300

    Updates

---
+++ precache-manifest.22a8029e30d4c24754e2ec6722d038cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a718fe7fdece59bc2b727220b042f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7768fe0b4937e2d2c4b",
+    "url": "/static/css/main.578d154b.chunk.css"
+  },
+  {
+    "revision": "599708912639a1056706",
+    "url": "/static/js/2.b3a7a706.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.b3a7a706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7768fe0b4937e2d2c4b",
+    "url": "/static/js/main.cf3e7c49.chunk.js"
+  },
+  {
+    "revision": "08889b2cf97449c74504",
+    "url": "/static/js/runtime-main.5ed940f4.js"
+  }
+]);
